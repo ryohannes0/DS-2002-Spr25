f0ce7091ddba396fea72a09f4304d7c66009bc18
Rebqa <ryohannes2018@gmail.com>
2025-04-15 14:54:16 +0000

    Updated

--- mywork/Lab_7/upload_file.sh
+++ mywork/Lab_7/upload_file.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-aws s3 cp local_file.jpg s3://my-first-bucket-aur8dm/
+AWS_REGION="us-east-1"
+
+aws s3 cp "$1" s3://"$2"/ --region "$AWS_REGION"
+aws s3 presign s3://"$2"/"$1" --expires-in "$3" --region "$AWS_REGION"
 
